3516d1b25ead908f418e274a9a7e09b658054d52
Kaier_Chou <469439801@qq.com>
2018-12-09 16:55:40 +0800

    通过koa views来管理pug

--- Koa/douban-trailer/server/index.js
+++ Koa/douban-trailer/server/index.js
@@ -1,18 +1,16 @@
 const Koa = require('koa')
 const app = new Koa()
-const ejs = require("ejs")
-const pug = require("pug")
-const {
-    htmlTpl,
-    ejsTpl,
-    pugTpl
-} = require('./tpl/index')
+const views =require('koa-views')
+const {resolve} = require('path')
+
+app.use(views(resolve(__dirname, './views'), {
+    extension: 'pug'
+}));
 
 app.use(async (ctx, next) => {
-    ctx.type = "text/html; charset=utf-8"
-    ctx.body = pug.render(pugTpl, {
-        "you": "Luke",
-        "me": "Kaier"
+    await ctx.render('index', {
+        you: "Luke",
+        me: 'Kaier'
     })
 })
 
